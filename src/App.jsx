4ba15f766dc65e90db18b82bb36bb588ba9ad7e4
Bipin Mahato <35443975+cyberbpeen@users.noreply.github.com>
2023-12-24 19:06:35 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/header";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Resume from "./components/resume";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Resume />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
